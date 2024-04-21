99b499cac1d52eca3cf5c9d040e6fdff9edcfb1c
B-Dmitriy <balyaevds.main@gmail.com>
2024-04-21 18:43:42 +0300

    feat: add database connection and config

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/B-Dmitriy/expenses
 
 go 1.22.2
+
+require github.com/go-sql-driver/mysql v1.8.1
+
+require (
+	filippo.io/edwards25519 v1.1.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,70 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+	"os"
+
+	"gopkg.in/yaml.v3"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type User struct {
+	ID              int
+	Username        string
+	Password        string
+	Email           string
+	Email_confirmed bool
+}
+
+type Config struct {
+	DBDriver string `yaml:"DB_DRIVER"`
+	DBName   string `yaml:"DB_NAME"`
+	DBUser   string `yaml:"DB_USER"`
+	DBPass   string `yaml:"DB_PASS"`
+}
+
+func readConfig() Config {
+	config := Config{}
+
+	file, err := os.ReadFile("config.yml")
+	if err != nil {
+		panic(err)
+	}
+
+	err = yaml.Unmarshal(file, &config)
+	if err != nil {
+		panic(err)
+	}
+
+	return config
+}
 
 func main() {
-	fmt.Println("expenses")
+	config := readConfig()
+	db, err := sql.Open(config.DBDriver, config.DBUser+":"+config.DBPass+"@/"+config.DBName)
+	if err != nil {
+		panic(err)
+	}
+
+	rows, err := db.Query("SELECT * FROM users;")
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+
+	users := make([]User, 0, 0)
+
+	for rows.Next() {
+		user := new(User)
+		if err := rows.Scan(&user.ID, &user.Username, &user.Password, &user.Email, &user.Email_confirmed); err != nil {
+			panic(err)
+		}
+		users = append(users, *user)
+	}
+
+	for _, v := range users {
+		fmt.Printf("User: %v\n", v)
+	}
 }
