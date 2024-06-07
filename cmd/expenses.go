00d8a58e0d92b70bd8ae9563cf8f564473ab0030
B-Dmitriy <balyaevds.main@gmail.com>
2024-06-07 20:11:59 +0300

    feat: logger

--- cmd/expenses.go
+++ cmd/expenses.go
@@ -2,32 +2,19 @@ package main
 
 import (
 	"log/slog"
-	"os"
+	"strconv"
 
 	"github.com/B-Dmitriy/expenses/internal/config"
-	"github.com/B-Dmitriy/expenses/internal/logger"
-	"github.com/B-Dmitriy/expenses/internal/storage/mysql"
+	"github.com/B-Dmitriy/expenses/pgk/logger"
 
-	muxHTTP "github.com/B-Dmitriy/expenses/internal/server/http"
-	_ "github.com/go-sql-driver/mysql"
+	server "github.com/B-Dmitriy/expenses/internal/server/http"
 )
 
 func main() {
-	config := config.MustLoad()
+	cfg := config.MustLoad()
 
-	logger := logger.SetupLogger(config.ENV)
-	logger.Info("logger initialized", slog.String("env", config.ENV))
+	lgr := logger.SetupLogger(cfg.ENV)
+	lgr.Info("logger initialized", slog.String("env", cfg.ENV), slog.String("port", strconv.Itoa(cfg.HTTPServer.Port)))
 
-	storage, err := mysql.NewMySQLStorage(&config.Storage)
-	if err != nil {
-		logger.Error("database connect error", slog.String("error", err.Error()))
-		os.Exit(1)
-	}
-
-	httpServer := muxHTTP.NewServer(config.HTTPServer, storage, logger)
-	err = httpServer.ListenAndServe()
-	if err != nil {
-		logger.Error(err.Error())
-		os.Exit(1)
-	}
+	server.NewServer(cfg.HTTPServer, lgr).Run()
 }
---
+++ pgk/web/parse.go
@@ -0,0 +1,38 @@
+package web
+
+import (
+	"net/http"
+	"net/url"
+	"strconv"
+)
+
+// ParseQueryParams return map containing query params with keys from names slice and error
+func ParseQueryParams(r *http.Request, names ...string) (map[string]string, error) {
+	queryParams, err := url.ParseQuery(r.URL.RawQuery)
+	if err != nil {
+		return nil, err
+	}
+
+	result := make(map[string]string, len(names))
+
+	for _, name := range names {
+		if len(queryParams[name]) != 0 {
+			result[name] = queryParams[name][0]
+		}
+	}
+
+	return result, nil
+}
+
+// ParseIDFromURL return URL path id parameter (integer) with name from second function argument
+// Example: "/user/{userID}" => id, err := ParseIDFromURL(r, "userID")
+func ParseIDFromURL(r *http.Request, name string) (int, error) {
+	idFromURL := r.PathValue(name)
+
+	param, err := strconv.Atoi(idFromURL)
+	if err != nil || param < 1 {
+		return 0, err
+	}
+
+	return param, nil
+}
